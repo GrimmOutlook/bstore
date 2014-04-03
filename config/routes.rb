0f67f1ddeb359c34539079728a324db2ff139030
GrimmOutlook <grimmoutlook87@gmail.com>
2014-04-03 15:46:02 -0400

    Included all resource routes in the admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Bstore::Application.routes.draw do
 
   namespace :admin do
-    get '/products'      => 'products#index', as: 'products'
-    get '/products/new'  => 'products#new', as: 'new_product'
-    get '/products/:id'  => 'products#show', as: 'product'
-    post '/products'     => 'products#create'
+    resources :products
+    # get '/products'      => 'products#index', as: 'products'
+    # get '/products/new'  => 'products#new', as: 'new_product'
+    # get '/products/:id'  => 'products#show', as: 'product'
+    # post '/products'     => 'products#create'
   end
 
   resources :subscriptions
