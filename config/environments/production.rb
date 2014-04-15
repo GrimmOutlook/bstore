da94a716ad38e5879aa6b4799b3df8f98db60e1c
GrimmOutlook <grimmoutlook87@gmail.com>
2014-04-15 19:23:25 -0400

    Added action_mailer settings for MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Bstore::Application.configure do
   config.action_mailer.default_url_options = {
     host: "http://shrouded-wave-5627.herokuapp.com/"
   }
+
+  config.action_mailer.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'http://shrouded-wave-5627.herokuapp.com/',
+    authentication: :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
 end
