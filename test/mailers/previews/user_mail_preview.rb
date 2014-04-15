448a7130c39020e63aa765e4e5f3988961ef7fe3
GrimmOutlook <grimmoutlook87@gmail.com>
2014-04-15 19:22:27 -0400

    Added class UserMailerPreview - duh.

--- test/mailers/previews/user_mail_preview.rb
+++ test/mailers/previews/user_mail_preview.rb
@@ -1,3 +1,7 @@
-def welcome
-  UserMailer.welcome(User.last)
+class UserMailerPreview < ActionMailer::Preview
+
+  # http://localhost:3000/rails/mailers/user_mailer/welcome
+  def welcome
+    UserMailer.welcome(User.last)
+  end
 end
